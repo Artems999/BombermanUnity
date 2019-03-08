6c35ff0a0cc262ac19418252461f980271d5ae18
Babichartem <Babichartemson@gmail.com>
2019-03-08 03:18:20 +0300

    Player Moves Refactored

--- GameDX/Assets/Scripts/PlayerMoves.cs
+++ GameDX/Assets/Scripts/PlayerMoves.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class PlayerMoves : MonoBehaviour
 {
     public GameObject Player;
-
-    public enum  Side
+    public enum Side
     {
         Left,
         Right,
@@ -14,61 +13,42 @@ public class PlayerMoves : MonoBehaviour
         Backward
     }
 
-    private IEnumerator coroutine;
-
     private Field field = new Field();
 
-    private bool KeyUp = true;
+   // private bool keyUp = true;
+    private bool stepFinished = true;
     private float MovingSpeed = 5.0f;
 
-    // Start is called before the first frame update
-
-    /* coroutine = WaitAndMove(0.1f);
-            StartCoroutine(coroutine);*/
-
-
-    // Update is called once per frame
     void Update()
     {
-        if (Input.anyKey)
+        if (stepFinished)
         {
-            if (KeyUp)
+            if (Input.GetKey(KeyCode.UpArrow))
             {
-                if (Input.GetKey(KeyCode.UpArrow))
-                {
-                    MovePlayer(Side.Forward);
-                }
-
-                if (Input.GetKey(KeyCode.DownArrow))
-                {
-                    MovePlayer(Side.Backward);
-                }
-
-                if (Input.GetKey(KeyCode.LeftArrow))
-                {
-                    MovePlayer(Side.Left);
-                }
-
-                if (Input.GetKey(KeyCode.RightArrow))
-                {
-                    MovePlayer(Side.Right);
-                }
-               
-                KeyUp = false;
+                MovePlayer(Side.Forward);
             }
-        }
-        else
-        {
-            KeyUp = true;
-        }
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                MovePlayer(Side.Backward);
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                MovePlayer(Side.Left);
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                MovePlayer(Side.Right);
+            }
 
+        }
     }
+
 
     void MovePlayer(Side side)
     {
-        Vector3 startPosition = new Vector3(GetPlayerPosition(0),1.0f,GetPlayerPosition(1));
+        Vector3 startPosition = new Vector3(GetPlayerPosition(0), 1.0f, GetPlayerPosition(1));
         Vector3 endPosition = startPosition;
-        
+
         switch (side)
         {
             case Side.Forward:
@@ -84,37 +64,35 @@ public class PlayerMoves : MonoBehaviour
                 endPosition.z -= 2;
                 break;
         }
+
         if (field.MovePlayer(side))
         {
-            coroutine = MoveFromTo(Player, startPosition, endPosition, MovingSpeed);
-            StartCoroutine(coroutine);
+            
+            StartCoroutine(MoveFromTo(Player, startPosition, endPosition, MovingSpeed));
         }
 
     }
 
-    
+
     IEnumerator MoveFromTo(GameObject objectToMove, Vector3 startPosition, Vector3 endPosition, float speed)
     {
+        stepFinished = false;
         float step = (speed / (startPosition - endPosition).magnitude) * Time.fixedDeltaTime;
         float t = 0;
         while (t <= 1.0f)
         {
-            t += step; 
-            objectToMove.transform.position = Vector3.Lerp(startPosition, endPosition, t); 
-            yield return new WaitForFixedUpdate();        
+            t += step;
+            objectToMove.transform.position = Vector3.Lerp(startPosition, endPosition, t);
+            yield return new WaitForFixedUpdate();
         }
+
         objectToMove.transform.position = endPosition;
+        stepFinished = true;
+
     }
 
     int GetPlayerPosition(int axis)
     {
         return Field.PlayerPosition[axis] * 2;
     }
-
-
-
-   
-
-  
-
 }
